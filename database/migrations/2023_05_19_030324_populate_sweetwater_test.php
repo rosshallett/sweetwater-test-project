5deba6e1564388e3991506285ba1ba3537dcf1cc
Ross Hallett <ross.d.hallett@gmail.com>
2023-05-19 01:35:06 -0400

    More progress on the migration
    Needs updated to consider the possibility of a comment including the `Expected Ship Date` text in the middle of the comment rather than just at the end.

--- database/migrations/2023_05_19_030324_populate_sweetwater_test.php
+++ database/migrations/2023_05_19_030324_populate_sweetwater_test.php
@@ -22,6 +22,20 @@ class PopulateSweetwaterTest extends Migration
         $contents = str_replace('0000-00-00 00:00:00', $date->format('Y-m-d H:i:s'), $contents);
 
         DB::unprepared($contents);
+
+        // Iterate over each comment in `sweetwater_test` to parse out expected ship date
+        $comments = DB::table('sweetwater_test')->get();
+
+        foreach ($comments as $row)
+        {
+            if (str_contains(($row->comments), 'Expected Ship Date:'))
+            {
+                $split = explode('Expected Ship Date: ', trim(preg_replace('/\s+/', ' ', $row->comments)));
+                $row->comments = trim($split[0]);
+                $row->shipdate_expected = DateTimeImmutable::createFromFormat('m/d/y', $split[1]);
+                DB::table('sweetwater_test')->where('orderid', $row->orderid)->update(array('comments' => $row->comments, 'shipdate_expected' => $row->shipdate_expected));
+            }
+        }
     }
 
     /**
